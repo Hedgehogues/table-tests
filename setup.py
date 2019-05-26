45ce0e5b9eefee02a274b9946d131949549f732e
Egor Urvanov <hedgehogues@bk.ru>
2019-05-27 01:40:26 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="table-tests",
+    version="0.0.1",
+    author="Egor Urvanov",
+    author_email="hedgehogues@bk.ru",
+    description="Simple engine for your tests. No code, no noodles",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Hedgehogues/table-tests",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7+",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
