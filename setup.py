8c1efd0459246ddba868cff2be4a24cdaea8b9c6
Egor <hedgehogues@bk.ru>
2019-07-08 01:06:25 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 import setuptools
-from pip.req import parse_requirements
+
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
 
 
 with open("README.md", "r") as fh:
