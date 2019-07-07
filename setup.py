d447f3efab35919b4f41eb4150615f63b31eefd1
Egor <hedgehogues@bk.ru>
2019-07-08 01:05:25 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 import setuptools
+from pip.req import parse_requirements
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+install_reqs = parse_requirements('./requirements.txt')
+reqs = [str(ir.req) for ir in install_reqs]
+    
 setuptools.setup(
     name="table-test",
     version="0.3.1",
@@ -18,4 +23,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=reqs,
 )
